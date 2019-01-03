069937c09317b4c6aa95b2007282fce8eaa77998
Durabun <eugenejon1@gmail.com>
2019-01-03 18:31:30 -0500

    new repo

---
+++ petStruct.cpp
@@ -0,0 +1,20 @@
+//This is a file to initialize the virtual pet
+
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+struct Pet {
+
+	string name;
+	int lvl;
+	int power;
+	float disposition;
+
+};
+
+int main(){
+	return 0;
+}
+

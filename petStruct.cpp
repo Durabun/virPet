188aae5b12ae7acf8603763e00e7b61df7282499
Durabun <eugenejon1@gmail.com>
2019-01-03 20:04:46 -0500

    adding to petStruct.cpp

--- petStruct.cpp
+++ petStruct.cpp
@@ -14,7 +14,32 @@ struct Pet {
 
 };
 
+void dispStats(int* Name);
+
 int main(){
+	
+	Pet Kaiju1;
+	Pet * Godzilla;
+	Godzilla = &Kaiju1;
+
+	Godzilla->name = "Goji";
+	Godzilla->lvl = 100;
+	Godzilla->power = 1000;
+	Godzilla->disposition = 100.0;
+
+//	dispStats(&Godzilla);
+	dispStats(0);
+
 	return 0;
 }
 
+void dispStats(int* Name){
+	*Name = 10;
+
+//	cout<<"Kaiju: "<<Name->name<<endl;
+//	cout<<"Level: "<<Name->lvl<<endl;
+//	cout<<"Power: "<<Name->power<<endl;
+//	cout<<"Disp.: "<<Name->disposition<<endl;
+
+	
+}
